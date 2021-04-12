8bd008ee501f64feb4726d2ec4d279c10c4059b5
s-ramdohkar <gretoefldiscussion@gmail.com>
2021-04-12 19:12:20 +0530

    Added add.

--- ArithmeticOps/Add/Program.cs
+++ ArithmeticOps/Add/Program.cs
@@ -6,7 +6,7 @@ namespace Add
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World Add!");
+            Console.WriteLine("Hello World Add Rakshita!");
         }
     }
 }
